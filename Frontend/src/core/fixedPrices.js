2ca6ffdbc8063408717d242fba9d41ab9fde9361
Amidu99 <amidushamika@gmail.com>
2025-03-17 12:49:27 +0530

    add project files

---
+++ Frontend/src/core/fixedPrices.js
@@ -0,0 +1,27 @@
+export const prices = [
+    {
+        _id: 0,
+        name: "Any",
+        array: []
+    },
+    {
+        _id: 1,
+        name: "Rs.0 to Rs.499",
+        array: [0, 499]
+    },
+    {
+        _id: 2,
+        name: "Rs.500 to Rs.999",
+        array: [500, 999]
+    },
+    {
+        _id: 3,
+        name: "Rs.1000 to Rs.4999",
+        array: [1000, 4999]
+    },
+    {
+        _id: 4,
+        name: "More than Rs.5000",
+        array: [5000, 10000]
+    }
+];
